853ea609f9b79fc158575812a6b16f64c7be8a0f
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2025-02-20 15:22:59 +0200

    add app assets

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,49 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesAvatar1
+  /// assets/images/avatar_1.svg
+  static const String imagesAvatar1 = "assets/images/avatar_1.svg";
+
+  /// Assets for imagesAvatar2
+  /// assets/images/avatar_2.svg
+  static const String imagesAvatar2 = "assets/images/avatar_2.svg";
+
+  /// Assets for imagesBalanceIcon
+  /// assets/images/balance_icon.svg
+  static const String imagesBalanceIcon = "assets/images/balance_icon.svg";
+
+  /// Assets for imagesCreditCard
+  /// assets/images/credit_card.svg
+  static const String imagesCreditCard = "assets/images/credit_card.svg";
+
+  /// Assets for imagesDashboardIcon
+  /// assets/images/dashboard_icon.svg
+  static const String imagesDashboardIcon = "assets/images/dashboard_icon.svg";
+
+  /// Assets for imagesExpenses
+  /// assets/images/expenses.svg
+  static const String imagesExpenses = "assets/images/expenses.svg";
+
+  /// Assets for imagesIncomeIcon
+  /// assets/images/income_icon.svg
+  static const String imagesIncomeIcon = "assets/images/income_icon.svg";
+
+  /// Assets for imagesInvestmentsIcon
+  /// assets/images/investments_icon.svg
+  static const String imagesInvestmentsIcon = "assets/images/investments_icon.svg";
+
+  /// Assets for imagesStatisticesIcon
+  /// assets/images/statistices_icon.svg
+  static const String imagesStatisticesIcon = "assets/images/statistices_icon.svg";
+
+  /// Assets for imagesTransactionIcon
+  /// assets/images/transaction_icon.svg
+  static const String imagesTransactionIcon = "assets/images/transaction_icon.svg";
+
+  /// Assets for imagesWalletIcon
+  /// assets/images/wallet_icon.svg
+  static const String imagesWalletIcon = "assets/images/wallet_icon.svg";
+}
+
